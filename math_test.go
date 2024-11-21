f4e7eb78465eef5e8fbf93240a2b6825881c8895
Rodrigo Freitas <fwtrodrigo@gmail.com>
2024-11-21 16:51:35 -0300

    feat: force error in the pipeline, for testing purposes.

--- math_test.go
+++ math_test.go
@@ -6,7 +6,7 @@ func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da soma é invaálido: Resultado %d. Esperado: %d", total, 30)
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
 
@@ -14,6 +14,14 @@ func TestSubtracao(t *testing.T) {
 	total := Subtracao(15, 15)
 
 	if total != 0 {
-		t.Errorf("Resultado da subtração é invaálido: Resultado %d. Esperado: %d", total, 30)
+		t.Errorf("Resultado da subtração é inválido: Resultado %d. Esperado: %d", total, 0)
+	}
+}
+
+func TestDivisao(t *testing.T) {
+	total := Divisao(15, 15)
+
+	if total != 0 {
+		t.Errorf("Resultado da divisao é inválido: Resultado %d. Esperado: %d", total, 1)
 	}
 }
